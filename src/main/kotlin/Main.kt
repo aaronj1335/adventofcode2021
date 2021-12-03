8d8c2060b28422b4eb221a7fc9807fc4c8909b2e
Aaron Stacy <aaronstacy@google.com>
2021-12-03 10:32:22 -0600

    day 3 q1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,9 +17,9 @@ fun day01() {
 fun day02() {
   fun String.magnitude() = split(' ')[1].toInt()
   fun String.toDelta() = when {
-    startsWith("forward") -> Pair(this.magnitude(), 0)
-    startsWith("up") -> Pair(0, -this.magnitude())
-    startsWith("down") -> Pair(0, this.magnitude())
+    startsWith("forward") -> Pair(magnitude(), 0)
+    startsWith("up") -> Pair(0, -magnitude())
+    startsWith("down") -> Pair(0, magnitude())
     else -> throw IllegalArgumentException("unexpected input: $this")
   }
   operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) = Pair(first + other.first, second + other.second)
@@ -28,7 +28,7 @@ fun day02() {
   fun Sequence<String>.q1() = map(String::toDelta).sum().product()
 
   fun Triple<Int, Int, Int>.product() = first * second
-  fun Triple<Int, Int, Int>.accumulateAimAndPosition(line: String): Triple<Int, Int, Int> = when {
+  fun Triple<Int, Int, Int>.accumulateAimAndPosition(line: String) = when {
     line.startsWith("forward") -> Triple( first + line.magnitude(), second + third * line.magnitude(), third)
     line.startsWith("down") -> Triple(first, second, third + line.magnitude())
     line.startsWith("up") -> Triple(first, second, third - line.magnitude())
@@ -40,7 +40,22 @@ fun day02() {
   run("day2input.txt", Sequence<String>::q2)
 }
 
+fun day03() {
+  val width = 12
+  operator fun List<Int>.plus(other: List<Int>) = zip(other).map { (i, j) -> i + j }
+  fun Sequence<String>.gamma(): Int = map { it.map(Char::digitToInt) }
+    .fold(Pair(0, List(width) { 0 })) { (count, frequenciesOfOnes), s -> Pair(count + 1, frequenciesOfOnes + s) }
+    .let { (count, frequenciesOfOnes) ->
+      frequenciesOfOnes.map { if (it > count / 2) 1 else 0 }.joinToString("").toInt(2)
+    }
+  fun Int.epsilon() = this xor List(width) { 1 }.joinToString("").toInt(2)
+  fun Sequence<String>.q1() = gamma().let { it * it.epsilon() }
+
+  run("day3input.txt", Sequence<String>::q1)
+}
+
 fun main(args: Array<String>) {
   day01()
   day02()
+  day03()
 }
